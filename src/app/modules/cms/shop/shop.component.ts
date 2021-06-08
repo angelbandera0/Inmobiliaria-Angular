b429b38d95ef11b5ab5d1c91a1f65257d08c1f2d
angelbandera0 <angelbandera0@gmail.com>
2021-06-08 12:14:04 -0400

    Poniendo funcional la vista

--- src/app/modules/cms/shop/shop.component.ts
+++ src/app/modules/cms/shop/shop.component.ts
@@ -1,27 +1,63 @@
+import { NotificationsToastrService } from 'src/app/services/notifications-toastr.service';
+import { Casa } from 'src/app/models/casa.model';
+import { CasaService } from 'src/app/services/casa.service';
 import { Component, OnInit } from '@angular/core';
-declare var activee:any;
+import { User } from 'src/app/models/user.model';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
+import { UserService } from 'src/app/services/user.service';
+declare var activee: any;
 // Declaro las variables de jQuery
-declare var jQuery:any;
-declare var $:any;
+declare var jQuery: any;
+declare var $: any;
 declare var myExtObject: any;
 declare var webGlObject: any;
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.css']
+  styleUrls: ['./shop.component.css'],
 })
 export class ShopComponent implements OnInit {
-
-  constructor() { }
+  casas: Casa[]=[];
+  user: User = new User();
+  constructor(
+    private casaService: CasaService,
+    private notificationsToastrService: NotificationsToastrService,
+    private userService: UserService,
+    private tokenStorageService: TokenStorageService
+  ) {}
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
     activee($);
-    jQuery(function() {
-      //alert('Hello, custom js');
-    });
-
+    const id = this.tokenStorageService.getId();
+    if(id){
+      this.userService.userById(id ? id : '').subscribe({
+        next: (res) => {
+          console.log(res);
+          this.user = res.user;
+          console.log(this.user?.myLikes);
+          this.casasRecientes();
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+    }else{
+      this.casasRecientes();
+    }
   }
-
+  casasRecientes(): void {
+    this.casaService.casaRecientes().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.casas = res.casas;
+        console.log(this.casas);
+      },
+      error: (res) => {
+        console.log(res);
+        this.notificationsToastrService.showSuccess(
+          'Ha ocurrido un error al cargar los datos de las casas recientes.'
+        );
+      },
+    });
+  }
 }
